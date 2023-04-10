80d8875c464be256195c625ddc615ffa5c14d8ac
Bailey Latimer <baileyl@beautyindustrygroup.com>
2023-04-09 23:04:03 -0700

    updated mobile styles

--- src/components/page-header.js
+++ src/components/page-header.js
@@ -3,7 +3,7 @@ import React from "react"
 const PageHeader = props => (
   
 <div className="mx-d mt-20 flex justify-center">
-<h1 className="text-xxl py-40">{props.title}</h1>
+<h1 className="text-xxl py-20 lg:py-40">{props.title}</h1>
 </div>
 );
 
--- src/components/project-card.js
+++ src/components/project-card.js
@@ -10,9 +10,9 @@ const ProjectCard = props => (
     <li className={props.classes + " reveal-fx reveal-fx--translate-up"}>
       <Link className=" project-card" to={props.url}>
         <StaticImage src="../images/img--01.jpg" alt={props.title} objectFit="cover" width="2000" />
-          <div class="flex flex-col lg:flex-row justify-between my-4 mx-4">
+          <div class="flex justify-between my-4 mx-4">
               <h1 class="">{props.title}</h1>
-              <h1 className="mt-4 lg:mt-0 color-contrast-medium"id="card-title-2">{props.desc}</h1>
+              <h1 className="lg:mt-0 color-contrast-medium"id="card-title-2">{props.desc}</h1>
           </div>
         </Link>
     </li>
