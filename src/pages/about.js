c63f8003ff1e0bf9d4098400a14f4ecd91c4a8ca
Bailey Latimer <baileyl@beautyindustrygroup.com>
2023-04-09 22:03:32 -0700

    updated about page

--- src/pages/about.js
+++ src/pages/about.js
@@ -6,6 +6,7 @@ import Layout from "../components/layout"
 import SEO from "../components/seo"
 
 import PageHeader from "../components/page-header"
+import TextCard from "../components/text-card"
 
 import HeroVideo from "../videos/hero.mp4"
 
@@ -13,6 +14,11 @@ const AboutPage = () => (
   <Layout>
     <SEO title="About" />
     <PageHeader title="About"/>
+    <StaticImage src="../images/ro-about.jpg" alt="Rocio Colomer Jorda • Dance" objectFit="cover" height="900" width="2000" />
+    <div className="flex flex-col lg:flex-row">
+      <TextCard text="STUDIO–RO is an award-winning dance practice working across a mix of disciplines that includes Heels, Hip hop, ballet, & Flaminco."/>
+      <TextCard text="Rocio Colomer Jorda has 15 years of dance experience. With influences from her home country of Spain, she now teaches in San Diego, CA."/>
+    </div>
   </Layout>
 )
 
