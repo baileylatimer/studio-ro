e9ea54e40c260b1f51368871882efd81dcba3781
Bailey Latimer <baileyl@beautyindustrygroup.com>
2023-04-09 21:35:28 -0700

    created build bundle

--- src/components/hero.js
+++ src/components/hero.js
@@ -5,7 +5,7 @@ const Hero = props => (
 <div class="hero">
 <h1 className="text-xxl text-white ro">RO</h1>
 <h1 className="text-xxl text-white cio">CIO</h1>
-<p className="hero-desc text-white">Rocio Colomer Jorda has 15 years of dance experience. With influences from her home country of spain, she now teaches in San Diego, CA.</p>
+<p className="hero-desc text-white ">Rocio Colomer Jorda has 15 years of dance experience. With influences from her home country of spain, she now teaches in San Diego, CA.</p>
 </div>
 );
 
