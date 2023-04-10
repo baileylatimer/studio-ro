34be2561a1bf484e1e6136fb2bafd6b38aba851c
Bailey Latimer <baileyl@beautyindustrygroup.com>
2023-04-10 00:22:25 -0700

    updated mobile contact spacing

--- src/pages/contact.js
+++ src/pages/contact.js
@@ -13,7 +13,7 @@ const ContactPage = () => (
   <Layout>
     <SEO title="Contact" />
     <PageHeader title="Contact"/>
-<div className="contact flex flex-col justify-center items-center pb-40 pt-40">
+<div className="contact flex flex-col justify-center items-center py-24 lg:py-40">
   <a tel="+1 (858) 776-8431">+1 (858) 776-8431</a>
   <a mailto="hello@rocio.dance">hello@rocio.dance</a>
 </div>
