6c8cd4fc48acfe8f60ceb17793e8171074cc1ee8
Bailey Latimer <baileyl@beautyindustrygroup.com>
2023-04-09 20:44:33 -0700

    added page titles

---
+++ src/components/page-header.js
@@ -0,0 +1,10 @@
+import React from "react"
+
+const PageHeader = props => (
+  
+<div className="mt-60 mx-d flex justify-center">
+<h1 className="text-xxl">{props.title}</h1>
+</div>
+);
+
+export default PageHeader
