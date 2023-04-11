0a7533ebcf72c10b3b015a88d07daab67c61bedb
Bailey Latimer <baileyl@beautyindustrygroup.com>
2023-04-10 19:44:27 -0700

    added ref to hero vid mobile

--- src/components/bg-video.js
+++ src/components/bg-video.js
@@ -1,17 +1,29 @@
-import React from "react"
+import React, { useRef } from "react";
 
+const BackgroundVideo = (props) => {
+  const videoRef = useRef(null);
 
-const BackgroundVideo= props => (
+  const handleUserGesture = () => {
+    const video = videoRef.current;
+    if (video) {
+      video.play();
+    }
+  };
+
+  return (
     <video
       className="background-video"
       autoPlay
       loop
       muted
       playsInline
-
+      ref={videoRef}
+      onClick={handleUserGesture} // Add user gesture event handler
+      onTouchStart={handleUserGesture} // Add user gesture event handler
     >
       <source src={props.video} type="video/mp4" />
     </video>
- );
+  );
+};
 
-export default BackgroundVideo
+export default BackgroundVideo;
