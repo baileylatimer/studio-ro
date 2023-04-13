af7de2617acb36b0f8a5d27c54b1347f64b194eb
Bailey Latimer <baileyl@beautyindustrygroup.com>
2023-04-12 23:19:19 -0700

    centered about img

--- src/pages/about.js
+++ src/pages/about.js
@@ -15,7 +15,9 @@ const AboutPage = () => (
   <Layout>
     <SEO title="About" />
     <PageHeader title="About"/>
+    <div className="flex w-full justify-center">
     <StaticImage src="../images/ro-about.jpg" alt="Rocio Colomer Jorda • Dance" objectFit="cover" height="1200" width="2000" />
+    </div>
     <div className="flex flex-col lg:flex-row">
       <TextCard text="STUDIO–RO is an award-winning dance practice working across a mix of disciplines that includes Heels, Hip hop, ballet, & Flaminco."/>
       <TextCard text="Rocio Colomer Jorda has 15 years of dance experience. With influences from her home country of Spain, she now teaches in San Diego, CA."/>
