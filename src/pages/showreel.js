3d67ddc2a5f47f825a71718c82fbc600204ca4d1
Bailey Latimer <baileyl@beautyindustrygroup.com>
2023-04-12 19:30:10 -0700

    functioning

--- src/pages/showreel.js
+++ src/pages/showreel.js
@@ -1,13 +1,10 @@
 import React from "react"
-import { Link } from "gatsby"
-import { StaticImage } from "gatsby-plugin-image"
 
 import Layout from "../components/layout"
 import SEO from "../components/seo"
 
 import PageHeader from "../components/page-header"
 import ProjectCard from "../components/project-card"
-import SectionTitle from "../components/section-title"
 import ContactBanner from "../components/contact-banner"
 
 import Heels2 from "../videos/heels-2.mp4"
