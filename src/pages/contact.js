1cf680ee3f52eb331d52b49484d30c167c6cfd28
Bailey Latimer <baileyl@beautyindustrygroup.com>
2024-04-21 19:38:23 -0700

    updated contact info

--- src/pages/contact.js
+++ src/pages/contact.js
@@ -15,7 +15,7 @@ const ContactPage = () => (
     <PageHeader title="Contact"/>
 <div className="contact flex flex-col lg:flex-row lg:gap-12 justify-center items-center py-24 lg:py-40">
   <a tel="+1 (323) 574-2422">US: +1 (323) 574-2422</a>
-  <a tel="+34 655647995">ES: +34 655647995</a>
+  <a tel="+34 655647995">ES: +34 655 647 995</a>
   <a mailto="hi@rocio.dance">hi@rocio.dance</a>
 </div>
 
